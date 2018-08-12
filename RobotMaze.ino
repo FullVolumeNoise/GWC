e6dbbafde97e0cd0018046556bc5fb021cda8994
FullVolumeNoise <41074743+FullVolumeNoise@users.noreply.github.com>
2018-08-12 14:02:35 -0500

    Add files via upload

---
+++ RobotMaze.ino
@@ -0,0 +1,47 @@
+int whisker1 = 7;
+int whisker2 = 5;
+int piezo = 13;
+int turns = 0;
+#include <Servo.h>
+#include <ArduinoRobot.h>
+Servo servoLeft;
+Servo servoRight;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(piezo, OUTPUT);
+  pinMode(whisker1, INPUT);
+  pinMode(whisker2, INPUT);
+  Serial.begin(9600);
+  servoLeft.attach(13);
+  servoRight.attach(12);
+  tone(piezo,600,500);
+  Robot.begin();
+  Robot.beginSpeaker();//Initialize the sound module
+  char aTinyMelody[] = "8eF-FFga4b.a.g.F.8beee-d2e.1-";// This is what we will play
+  Robot.playMelody(aTinyMelody);// Play the melody 
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  //tone(piezo, 600, 500);
+  noTone(piezo);
+  if(digitalRead(whisker1)==0 || digitalRead(whisker2)==0){
+    Serial.println("whisker pressed");
+    servoLeft.writeMicroseconds(1700);
+    servoRight.writeMicroseconds(1300);
+    delay (800);
+    servoLeft.writeMicroseconds(1300);
+    servoRight.writeMicroseconds(1300);
+    delay (650);
+    turns ++; 1;
+  }else{
+    Serial.println("whisker not pressed");
+    servoLeft.writeMicroseconds(1300);
+    servoRight.writeMicroseconds(1700);
+  }
+  if(digitalRead(whisker1)==0 || digitalRead(whisker2)==0) {
+    tone(piezo,600,500);
+  }
+}
+
+
---
+++ robotherekittykitty.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>                           // Include servo library
+#include <ArduinoRobot.h> 
+
+Servo servoRight;
+Servo servoLeft;
+
+void moveKitty() {
+  // Your Code Here
+}
+
+void stopKitty() {
+  // Your Code Here
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Robot.begin();
+  Serial.print("we are");
+  Serial.println("We are");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   // If the robot is blocked, turn until free
+  while (getDistance() < 40) { // If an obstacle is less than 20cm away
+    setFace(false); //shows an unhappy face
+    Robot.motorsStop(); // stop the motors
+    delay(1000); // wait for a moment
+    Robot.turn(90); // turn to the right and try again
+    setFace(true); // happy face
+  }
+  // if there are no objects in the way, keep moving
+  Robot.motorsWrite(255, 255);
+  delay(100);
+}
